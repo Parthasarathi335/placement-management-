ea7c1418f7929ba08cc8e7572d3bf3fea5bed20c
Partha Sarathi <parthasarathi1101@gmail.com>
2026-09-17 17:43:44 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
+};
+module.exports = nextConfig;
